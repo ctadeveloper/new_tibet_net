0b963322c03f26cb44f108ee63974a44662623a8
Tenzin Rinzin <rinzin@Tenzins-iMac.local>
2020-12-05 11:24:18 +0530

    trying to fix the slider new height issue

--- templates/home/content-featured_news.php
+++ templates/home/content-featured_news.php
@@ -47,18 +47,41 @@
                 'paged' => $paged
             ));
             while ($flash_news_loop->have_posts()) : $flash_news_loop->the_post();
-                // Thumbnail URL
                 $thumb_url = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
-                // Title Excert
-                $thumb1 = thumbResizeIM($thumb_url, 70, 70, get_the_ID());
+                // echo $default;
+                // ImgMagick
+                // $thumb1 = thumbResizeIM($thumb_url, 280, 140, get_the_ID());
+                $cta_has_thumb = '';
+                $post_index_img = cta_thumb(60, 60);
+                if ($post_index_img != '') {
+                    $img_html = '<img class="rounded w-100 rounded-top lazyload blur-up" data-src="' . $post_index_img . '" alt="' . get_the_title() . '">' . "\r\n";
+                } else { // if (!is_page_template( 'page-homepage.php' )) {
+                    $img_html = '<img class="rounded w-100 rounded-top lazyload blur-up" data-src="' . get_template_directory_uri() . '/img/cta_grid_default-280x140.jpg" height="140" width="280" alt="' . get_the_title() . '"' . "\r\n";
+                }
             ?>
                 <div class="row">
-                    <div class="col-12">
-                        <div class="media my-1">
+                        <div class="col-md-2 px-0 col-3">
                             <a href="<?php echo get_permalink(); ?>">
-                                <img data-src="<?php echo $thumb1; ?>" alt="<?php echo esc_html(get_the_title()); ?>" class="img-responsive rounded mr-3 lazyload blur-up">
+                                <?php echo $img_html; ?>
                             </a>
-                            <div class="media-body">
+                        </div>
+                        <div class="col-md-10 col-9">
+                            <a href="<?php echo get_permalink(); ?>">
+                                <h6 class="font-weight-bolder"><?php echo excerpt_title_length(75) ?></h6>
+                            </a>
+                            <p class="small text-muted p-0 m-0"><?php echo get_the_time("F j, Y"); ?>
+                                <?php include(TEMPLATEPATH . '/templates/social_share_api.php'); ?>
+                            </p>
+                            <!-- <p class="text-muted m-0 p-0 d-none d-md-block"><?php echo get_print_excerpt(100).'...' ?></p> -->
+                        </div>
+                    <!-- <div class="col-3">
+                    
+                            <a href="<?php echo get_permalink(); ?>">
+                                <img data-src="<?php echo $thumb1; ?>" alt="<?php echo esc_html(get_the_title()); ?>" class="w-100 rounded mr-3 lazyload blur-up">
+                            </a>
+                    </div> -->
+
+                    <!-- <div class="col-9">
                                 <a href="<?php echo get_permalink(); ?>">
                                     <h6 class="mt-0 font-weight-bolder"><?php echo excerpt_title_length(50) ?></h6>
                                 </a>
@@ -67,7 +90,7 @@
                                 </p>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             <?php endwhile;
             wp_reset_postdata();
