41b05246593d0f4339a8844b127e7687218eec03
Tenzin Rinzin <tcrc@tcrcs-MacBook-Air.local>
2020-12-24 14:36:05 +0530

    fixing department page

--- page-department.php
+++ page-department.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Template Name: Deparment
  * The template for displaying all pages
  *
  * This is the template that displays all pages by default.
@@ -45,67 +46,21 @@ $container = get_theme_mod( 'understrap_container_type' );
         $pfgallery_items .= '</a>'. "\r\n";
         $pfgallery_items .= '</div>'. "\r\n"; // end item 
         */
-  
-    if (get_field('dep_slider_images')){
+  ?>
+        <?php
+        while ( have_posts() ) {
+            the_post();
+            get_template_part( 'loop-templates/content', 'page' );
+        }
+        ?>
 
-            $slider_imgs_acc = '';
-            $slider_img_num = 0;
-                
-            while(the_repeater_field('dep_slider_images')): 
-
-                $slider_img = get_sub_field('dep_slider_image');
-                // $slider_img_thumb = thumbResizeIM($slider_img, 150, 150);
-                // $slider_imgs_acc  .= '<li><a href="#"><img src="' . $slider_img . '" alt="image01" /></a></li>';
-                $slider_imgs_acc  .= '<li>'. "\r\n";
-                $slider_imgs_acc .= '<a class="jackbox" data-group="slider_imgs" data-thumbnail="' . $slider_img . '" '; 
-        		$slider_imgs_acc .= 'data-title="' . sanitize_text_field(get_the_title()) . '" data-width="1100" '; 
-        		$slider_imgs_acc .= 'data-description="" '; 
-        		$slider_imgs_acc .= 'href="' . $slider_img . '">'. "\r\n";
-        		$slider_imgs_acc .= '<div class="jackbox-hover jackbox-hover-black jackbox-hover-magnify"></div>'. "\r\n";
-                $slider_imgs_acc  .= '<img src="' . $slider_img . '" alt="Image 1">'. "\r\n";
-                $slider_imgs_acc .= '</a>'. "\r\n";
-                $slider_imgs_acc  .= '</li>'. "\r\n";   
-
-            endwhile; 
-            ?>
-
-         <div class="row">	   
-
-		<div id="department_jcarousel" class="jcarousel-wrapper">
-	        <div class="jcarousel">
-	            <ul>
-	                
-	            	<?php echo $slider_imgs_acc; ?>
-
-	            </ul>
-	        </div>
-
-	        <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
-	        <a href="#" class="jcarousel-control-next">&rsaquo;</a>
-
-	        <p class="jcarousel-pagination"></p>
-	    </div>
-
-	    </div>
-
-    <?php  }  ?>
-
- 	
-
-				<?php
-				while ( have_posts() ) {
-					the_post();
-					get_template_part( 'loop-templates/content', 'page' );
-				}
-				?>
-
-				<?php 
-					if(get_field('about_tab_items')){
-						echo true;
-					}else{
-						echo false;
-					}
-				?>
+        <?php 
+            if(get_field('about_tab_items')){
+                echo true;
+            }else{
+                echo false;
+            }
+        ?>
 
 			</main><!-- #main -->
 		</div><!-- .row -->
